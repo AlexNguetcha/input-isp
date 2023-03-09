2d2f0d44c62ad29592b1c373287c2891cf2466bb
AlexNguetcha <nguetchaalex@gmail.com>
2023-03-09 11:10:17 +0100

    add other default props values

--- src/components/InputIsp.tsx
+++ src/components/InputIsp.tsx
@@ -4,74 +4,77 @@ import Loader from './Loader';
 import './InputIsp.css';
 
 export default function InputIsp({ initialPhoneNumber, onChange, inputClassName, containerClassName }) {
-  const PHONE_NUMBER_ISP_REGEX = {
-    ORANGE_CM: /^\+2376(55|56|57|58|59|9\d{1})\d{6}$/,
-    MTN_CM: /^\+2376(50|51|52|53|54|80|81|82|83|7\d{1})\d{6}$/,
-    CAMTEL_CM: /^\+237620\d{6}$/,
-  };
-  const REMOTE_IMAGE_BASE_URL = 'https://raw.githubusercontent.com/AlexNguetcha/input-isp/dev/src/assets/logo/'
+    const PHONE_NUMBER_ISP_REGEX = {
+        ORANGE_CM: /^\+2376(55|56|57|58|59|9\d{1})\d{6}$/,
+        MTN_CM: /^\+2376(50|51|52|53|54|80|81|82|83|7\d{1})\d{6}$/,
+        CAMTEL_CM: /^\+237620\d{6}$/,
+    };
+    const REMOTE_IMAGE_BASE_URL = 'https://raw.githubusercontent.com/AlexNguetcha/input-isp/dev/src/assets/logo/'
 
-  // Declare state variables
-  const [detectedIsp, setDetectedIsp] = useState(null);
-  const [phoneNumber, setPhoneNumber] = useState(initialPhoneNumber);
-  const [isLoading, setIsLoading] = useState(true);
+    // Declare state variables
+    const [detectedIsp, setDetectedIsp] = useState(null);
+    const [phoneNumber, setPhoneNumber] = useState(initialPhoneNumber);
+    const [isLoading, setIsLoading] = useState(true);
 
-  // Memoize the result of detecting the ISP from the phone number
-  const detectIsp = useMemo(() => {
-    for (const isp in PHONE_NUMBER_ISP_REGEX) {
-      if (PHONE_NUMBER_ISP_REGEX[isp].test(phoneNumber) === true) {
-        return isp;
-      }
-    }
-    return null;
-  }, [phoneNumber, PHONE_NUMBER_ISP_REGEX]);
+    // Memoize the result of detecting the ISP from the phone number
+    const detectIsp = useMemo(() => {
+        for (const isp in PHONE_NUMBER_ISP_REGEX) {
+            if (PHONE_NUMBER_ISP_REGEX[isp].test(phoneNumber) === true) {
+                return isp;
+            }
+        }
+        return null;
+    }, [phoneNumber, PHONE_NUMBER_ISP_REGEX]);
 
-  // Update the detected ISP when the phone number changes
-  useEffect(() => {
-    setDetectedIsp(detectIsp);
-  }, [detectIsp]);
+    // Update the detected ISP when the phone number changes
+    useEffect(() => {
+        setDetectedIsp(detectIsp);
+    }, [detectIsp]);
 
-  // Handle changes to the phone number input
-  const handlePhoneNumberChange = (event) => {
-    onChange(event.target.value)
-    setPhoneNumber(event.target.value);
-  };
+    // Handle changes to the phone number input
+    const handlePhoneNumberChange = (event) => {
+        onChange(event.target.value)
+        setPhoneNumber(event.target.value);
+    };
 
-  // Construct the URL for the ISP logo based on the detected ISP
-  const ispLogo = `${REMOTE_IMAGE_BASE_URL}${(detectedIsp ?? '').toLowerCase()}.jpg`;
+    // Construct the URL for the ISP logo based on the detected ISP
+    const ispLogo = `${REMOTE_IMAGE_BASE_URL}${(detectedIsp ?? '').toLowerCase()}.jpg`;
 
-  return (
-    <div className={"InputIsp" + (` ${containerClassName}` ?? "")}>
-      {/* Render the phone number input */}
-      <input {...(inputClassName ? { className: inputClassName } : {})} type="tel" value={phoneNumber} onChange={handlePhoneNumberChange} />
+    return (
+        <div className={"InputIsp" + (` ${containerClassName}` ?? "")}>
+            {/* Render the phone number input */}
+            <input {...(inputClassName ? { className: inputClassName } : {})} type="tel" value={phoneNumber} onChange={handlePhoneNumberChange} />
 
-      {/* Render the ISP logo, with a loader if it is still loading */}
-      <div className="InputIsp__Logo">
-        {detectIsp !== null && isLoading && <Loader />}
-        {detectedIsp && (
-          <img
-            onLoad={() => setIsLoading(false)}
-            onError={() => setIsLoading(false)}
-            src={ispLogo}
-            alt={`${detectedIsp} logo`}
-            title={detectedIsp}
-            style={{ display: isLoading ? "none" : "block" }}
-          />
-        )}
-      </div>
-    </div>
-  );
+            {/* Render the ISP logo, with a loader if it is still loading */}
+            <div className="InputIsp__Logo">
+                {detectIsp !== null && isLoading && <Loader />}
+                {detectedIsp && (
+                    <img
+                        onLoad={() => setIsLoading(false)}
+                        onError={() => setIsLoading(false)}
+                        src={ispLogo}
+                        alt={`${detectedIsp} logo`}
+                        title={detectedIsp}
+                        style={{ display: isLoading ? "none" : "block" }}
+                    />
+                )}
+            </div>
+        </div>
+    );
 }
 
 // Declare the prop types for the InputIsp component
 InputIsp.propTypes = {
-  initialPhoneNumber: PropTypes.string.isRequired,
-  onChange: PropTypes.func,
-  inputClassName: PropTypes.string,
-  containerClassName: PropTypes.string
+    initialPhoneNumber: PropTypes.string,
+    onChange: PropTypes.func,
+    inputClassName: PropTypes.string,
+    containerClassName: PropTypes.string
 };
 
 // Set default props for the InputIsp component
 InputIsp.defaultProps = {
-  onChange: (value) => { }
+    initialPhoneNumber: '',
+    onChange: (value) => { },
+    inputClassName: null,
+    containerClassName: null,
 };
