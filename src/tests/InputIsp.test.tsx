bc8e411deeaba26ae6b9bc86be8b3931b1854375
Alex Nguetcha <nguetchaalex@gmail.com>
2023-03-25 10:13:43 +0100

    Add jest typescript support

--- src/tests/InputIsp.test.tsx
+++ src/tests/InputIsp.test.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
-import { render, fireEvent } from '@testing-library/react';
 import InputIsp from '../components/InputIsp';
-
+import '@testing-library/jest-dom'
+import {render, fireEvent, screen, waitFor} from '@testing-library/react';
 
 describe('InputIsp', () => {
   test('renders an input element', () => {
